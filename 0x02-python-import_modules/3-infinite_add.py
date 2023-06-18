e67266607e0daf8359e88aaefc6d019bacdb5443
Nathlaine <nathlainematilda@gmail.com>
2023-06-18 07:42:17 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
 
-    complete  = 0
+    complete = 0
     for i in range(len(sys.argv) - 1):
         complete += int(sys.argv[i + 1])
     print("{}".format(complete))
